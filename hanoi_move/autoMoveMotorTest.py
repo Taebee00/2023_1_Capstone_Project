b3afe1c1e26aff8501d337b09aefe12c070a1920
sw801733 <sw801733@naver.com>
2023-05-12 16:35:08 +0900

    aa

--- hanoi_move/autoMoveMotorTest.py
+++ hanoi_move/autoMoveMotorTest.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     # while True:
     current_state = [[1, 2, 3], [], []]
     coordinates = [
-        [[11, 2.2, -1.8], [11.2, 2.2, 2.5], [13.4, -4.5, 2.1]],
+        [[11, 2.2, -1.8], [11.2, 2.2, 2.5], [13.3, -4.5, 2.1]],
         [[11, 2.2, -2.5], [11.2, 2.2, 2.3], [11, -0.9, 2.3]],
         [[13.4, -4.5, -3.4], [13.4, -4.5, 2.5], [11.9, -0.8, 2.5]],
-        [[12.3, 2.1, -3], [12.5, 2.1, 2.5], [14, -4.8, 2.1]],
+        [[12.3, 2.1, -3.3], [12.5, 2.1, 2.5], [14, -4.8, 2.1]],
         [[11, -1, -2.5], [11.3, -1, 2.5], [11.4, 2.1, 2.3]],
         [[11, -1, -3], [11.3, -1, 2.5], [13.5, -4.7, 2.2]],
-        [[11, 2.1, -3], [11.3, 2.2, 2.4], [13.2, -4.4, 2.2]],
+        [[11, 2.1, -3], [11.3, 2.2, 2.4], [13.1, -4.5, 2.2]],
     ]
     h = HanoiTower(3)
 
@@ -59,7 +59,7 @@ if __name__ == '__main__':
             for j in range(3):
                 if j == 1 :
                     cm.gripperMove(gripper_angle)
-                    time.sleep(1)
+                    time.sleep(1.5)
 
                 theta_0, theta_1, theta_2 = CalculateTheta(
                     coordinates[i - 1][j][0], coordinates[i - 1][j][1], coordinates[i - 1][j][2])
@@ -68,17 +68,17 @@ if __name__ == '__main__':
                 cm.target_angles = [theta_0, theta_1, theta_2]
                 print(theta_0, theta_1, theta_2)
                 cm.moveArmSlow()
-                time.sleep(1)
+                time.sleep(1.5)
 
             theta_0, theta_1, theta_2 = CalculateTheta(
                 coordinates[i - 1][2][0], coordinates[i - 1][2][1], 1)
             cm.target_angles = [theta_0, theta_1, theta_2]
             cm.moveArmSlow()
-            time.sleep(1)
+            time.sleep(1.5)
             cm.gripperMove(0)
             
-            time.sleep(1)
+            time.sleep(1.5)
             cm.setDefault()
-            time.sleep(1)
+            time.sleep(1.5)
 
     print(h.current_state_idx)
