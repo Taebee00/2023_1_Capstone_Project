db8c90dc041978372b3b0c2c8f42cfedc1d8c4bc
sw801733 <sw801733@naver.com>
2023-05-19 14:34:43 +0900

    simplify autoMoveMotorMoveTest.py

--- hanoi_move/autoMoveMotorTest.py
+++ hanoi_move/autoMoveMotorTest.py
@@ -2,6 +2,38 @@ from controlMotor import *
 from utils import *
 from hanoi import *
 
+
+GRIPPER_COLORS = {1: 110, 2: 100, 3: 90}
+
+
+def get_pole_idx(letter):
+    return ord(letter) - ord('A')
+
+
+def get_wonban(state, pole_letter):
+    return state[get_pole_idx(pole_letter)][0] if state[get_pole_idx(pole_letter)] else None
+
+
+def calculate_and_move(cm, coord, is_grip, gripper_angle):
+    theta_0, theta_1, theta_2 = CalculateTheta(*coord)
+    print(*coord)
+    cm.target_angles = [theta_0, theta_1, theta_2]
+    print(theta_0, theta_1, theta_2)
+    cm.moveArmSlow()
+    if is_grip:
+        cm.gripperMove(gripper_angle)
+        time.sleep(1)
+
+
+def execute_movement(cm, coordinates, gripper_angle):
+    for coord in coordinates:
+        calculate_and_move(cm, coord, coord[1] == 1, gripper_angle)
+    cm.gripperMove(0)
+    time.sleep(1)
+    cm.setDefault()
+    time.sleep(1)
+
+
 if __name__ == '__main__':
     cm = ControlMotor()
     cm.setDefault()
@@ -10,7 +42,6 @@ if __name__ == '__main__':
     Green = 90
 
     # while True:
-    current_state = [[1, 2, 3], [], []]
     coordinates = [
         [[11, 2.2, -1.8], [11.2, 2.2, 2.5], [13.4, -4.5, 2.1]],
         [[11, 2.2, -2.5], [11.2, 2.2, 2.3], [11, -0.9, 2.3]],
@@ -20,65 +51,15 @@ if __name__ == '__main__':
         [[11, -1, -3], [11.3, -1, 2.5], [13.5, -4.7, 2.2]],
         [[11, 2.1, -3], [11.3, 2.2, 2.4], [13.2, -4.4, 2.2]],
     ]
+    current_state = [[1, 2, 3], [], []]
     h = HanoiTower(3)
-
     h.invade_state(current_state)
 
-    for i in range(len(h.state_history)):
-        if i == h.current_state_idx:
-            print(f"--> {h.state_history[i]}")
-        else:
-            print(h.state_history[i])
-            if h.state_history[i][3][1] == 'A':
-                wonban = h.state_history[i][0][0]
-                if wonban == 1:
-                    gripper_angle = Yellow
-                elif wonban == 2:
-                    gripper_angle = Red
-                elif wonban == 3:
-                    gripper_angle = Green
-
-            elif h.state_history[i][3][1] == 'B':
-                wonban = h.state_history[i][1][0]
-                if wonban == 1:
-                    gripper_angle = Yellow
-                elif wonban == 2:
-                    gripper_angle = Red
-                elif wonban == 3:
-                    gripper_angle = Green
-
-            elif h.state_history[i][3][1] == 'C':
-                wonban = h.state_history[i][2][0]
-                if wonban == 1:
-                    gripper_angle = Yellow
-                elif wonban == 2:
-                    gripper_angle = Red
-                elif wonban == 3:
-                    gripper_angle = Green
-
-            for j in range(3):
-                if j == 1 :
-                    cm.gripperMove(gripper_angle)
-                    time.sleep(1)
-
-                theta_0, theta_1, theta_2 = CalculateTheta(
-                    coordinates[i - 1][j][0], coordinates[i - 1][j][1], coordinates[i - 1][j][2])
-                print(coordinates[i - 1][j][0], coordinates[i - 1]
-                      [j][1], coordinates[i - 1][j][2])
-                cm.target_angles = [theta_0, theta_1, theta_2]
-                print(theta_0, theta_1, theta_2)
-                cm.moveArmSlow()
-                time.sleep(1)
-
-            theta_0, theta_1, theta_2 = CalculateTheta(
-                coordinates[i - 1][2][0], coordinates[i - 1][2][1], 1)
-            cm.target_angles = [theta_0, theta_1, theta_2]
-            cm.moveArmSlow()
-            time.sleep(1)
-            cm.gripperMove(0)
-            
-            time.sleep(1)
-            cm.setDefault()
-            time.sleep(1)
+    for i, state in enumerate(h.state_history):
+        print(f"--> {state}" if i == h.current_state_idx else state)
+        if i != h.current_state_idx:
+            gripper_angle = GRIPPER_COLORS.get(get_wonban(state, state[3][1]))
+            if gripper_angle is not None:
+                execute_movement(cm, coordinates[i - 1], gripper_angle)
 
     print(h.current_state_idx)
